10a6320f710ccd31e7b4f2285de4ee6a9c8fa0cb
sanika <sanika@umich.edu>
2019-05-20 20:49:03 -0400

    Added setup files

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './whosThatPokemon/main.jsx',
+  output: {
+    path: path.join(__dirname, '/whosThatPokemon/static/js/'),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        // Test for js or jsx files
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        query: {
+          // Convert ES6 syntax to ES5 for browser compatibility
+          presets: ['env', 'react'],
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
+
